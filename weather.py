5bdf148aa31b45039c1cc581c40f1f41eb6209dd
yuchu <2102041310@st.nuc.edu.cn>
2025-03-03 19:20:15 +0800

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,8 +3,7 @@ import os
 import aiohttp
 from typing import Optional
 from rapidfuzz import process, fuzz
-from app.plugin.plugin_base import PluginBase
-from app.logger import logger
+from qfurina.api import PluginBase, logger
 from .city_codes import get_city_code, get_city_name, CITY_CODES
 
 @PluginBase.register("weather")
